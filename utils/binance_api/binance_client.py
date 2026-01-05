90aab51e0e92925dc62ece5050b301cb24af7e59
dort5bot <dersdep@gmail.com>
2026-01-05 18:29:18 +0300

    Delete utils/binance_api directory

--- utils/binance_api/binance_client.py
+++
@@ -1,8 +0,0 @@
-# utils/binance_api/binance_client.py
-"""
-(Yani artık hiçbir mantık içermez, sadece alias’dır. Böylece modül içi değişiklikler kırılmaz.)
-
-Tüm handler ve jobs kodları artık BinanceAggregator üzerinden veri alır
-"""
-from .binance_request import BinanceHTTPClient
-__all__ = ["BinanceHTTPClient"]
